178a5f5cfd0d736c3bad19e10b68716fbf86b813
satonaka-chie9 <satonaka-chie9@users.noreply.github.com>
2025-04-23 04:46:14 +0000

    deploy: ba2ae5e3ee75b6d26ce3d13f7df6bc669446ce94

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "5966f1df7c39aabe977ac10ecf78a536",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "f0e99ec03f80b4ae857e13494178a2ed"};
+"flutter_bootstrap.js": "18ef4667db75c227550b0ed99085d121"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
